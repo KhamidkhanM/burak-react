776786fb86c308f5c19f307fd90e7f80267347d9
KhamidkhanM <HoshimalievHamidxonn@gmail.com>
2026-06-30 16:14:28 +0900

    feat: first part of details section

--- src/app/components/headers/HomeNavBar.tsx
+++ src/app/components/headers/HomeNavBar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import "../../../css/navbar.css";
 
 export function HomeNavbar() {
-  const authMember = true;
+  const authMember = null;
 
   return (
     <div
@@ -51,24 +51,37 @@ export function HomeNavbar() {
 
             {!authMember ? (
               <Box>
-                <Button
-                  variant="contained"
-                  className = "login-button"
-                >
+                <Button variant="contained" className="login-button">
                   Login
                 </Button>
               </Box>
             ) : (
               <img
-              className = "user-avatar"
+                className="user-avatar"
                 src="/icons/default-user.svg"
                 alt="user avatar"
-                
               />
             )}
           </Stack>
         </Stack>
-        <Stack>DETAIL</Stack>
+
+        <Stack className={"header-frame"}>
+          <Stack className={"detail"}>
+            <Box className={"head-main-txt"}>
+              World's Most Delicious Cousine
+            </Box>
+            <Box className={"wel-txt"}>The Choice, not just a choice</Box>
+            <Box className={"service-txt"}>24 hours service</Box>
+            <Box className={"signup"}>
+              {!authMember ? (
+                <Button variant={"contained"} className={"signup-button"}>
+                  SIGN UP
+                </Button>
+              ) : null}
+            </Box>
+          </Stack>
+          <Stack className={"logo-frame"}>LOGO</Stack>
+        </Stack>
       </Container>
     </div>
   );
