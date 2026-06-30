6ff1865a141b4836a25f28a351737db98045549b
KhamidkhanM <HoshimalievHamidxonn@gmail.com>
2026-06-30 15:41:53 +0900

    fix: homeNavBar menu part refactoring

--- src/app/components/headers/HomeNavBar.tsx
+++ src/app/components/headers/HomeNavBar.tsx
@@ -3,68 +3,68 @@ import { NavLink } from "react-router-dom";
 import "../../../css/navbar.css";
 
 export function HomeNavbar() {
-  const authMember = null;
+  const authMember = true;
 
   return (
     <div
       className="home-navbar"
-      style={{
-        background: `url(/img/banner.webp) center/cover no-repeat`,
-      }}
+      style={{ background: `url(/img/banner.webp) center/cover no-repeat` }}
     >
-      <Container sx={{ mt: "55px", height: "642px" }}>
-        <Stack
-          sx={{
-            height: "50px",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
+      <Container className="navbar-container">
+        <Stack className="menu">
           <Box>
             <NavLink to="/">
-              <img
-                style={{ width: "125px", height: "30px" }}
-                src="/icons/burak.svg"
-                alt="logo"
-              />
+              <img className="brand-logo" src="/icons/burak.svg" alt="logo" />
             </NavLink>
           </Box>
-          <Stack
-            sx={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              minWidth: "700px",
-              alignItems: "center",
-            }}
-          >
-            <Box className = "hover-line">
-              <NavLink to="/" activeClassName= {"underline"}>Home</NavLink>
+          <Stack className="links">
+            <Box className={"hover-line"}>
+              <NavLink to="/" activeClassName={"underline"}>
+                Home
+              </NavLink>
             </Box>
-            <Box className = "hover-line">
-              <NavLink to="/products" activeClassName= {"underline"}>Products</NavLink>
+            <Box className={"hover-line"}>
+              <NavLink to="/products" activeClassName={"underline"}>
+                Products
+              </NavLink>
             </Box>
             {authMember ? (
-              <Box className = "hover-line">
-                <NavLink to="/orders" activeClassName= {"underline"}>Orders</NavLink>
+              <Box className={"hover-line"}>
+                <NavLink to="/orders" activeClassName={"underline"}>
+                  Orders
+                </NavLink>
               </Box>
             ) : null}
             {authMember ? (
-              <Box className = "hover-line">
-                <NavLink to="/member-page" activeClassName= {"underline"}>My Page</NavLink>
+              <Box className={"hover-line"}>
+                <NavLink to="/member-page" activeClassName={"underline"}>
+                  My Page
+                </NavLink>
               </Box>
             ) : null}
-            <Box className = "hover-line">
-              <NavLink to="/help" activeClassName= {"underline"}>Help</NavLink>
+            <Box className={"hover-line"}>
+              <NavLink to="/help" activeClassName={"underline"}>
+                Help
+              </NavLink>
             </Box>
             {/* BASKET */}
 
             {!authMember ? (
               <Box>
-                <Button variant = "contained" style = {{background: "#3776CC", color: "#8f8f88"}}>Login</Button>
+                <Button
+                  variant="contained"
+                  className = "login-button"
+                >
+                  Login
+                </Button>
               </Box>
             ) : (
-              <img alt="" />
+              <img
+              className = "user-avatar"
+                src="/icons/default-user.svg"
+                alt="user avatar"
+                
+              />
             )}
           </Stack>
         </Stack>
