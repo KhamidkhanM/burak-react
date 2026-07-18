26bd1442f0d55b7706be821041b888401ac4cf3d
KhamidkhanM <hoshimalievhamidxonn@gmail.com>
2026-07-18 16:50:08 +0900

    feat: create HomePage Redux configuration

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,14 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import reduxLogger from "redux-logger";
+import homePageReducer from "./screens/homePage/slice";
 
 export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     // @ts-ignore
     getDefaultMiddleware().concat(reduxLogger),
-  reducer: {},
+  reducer: {
+    homePage: homePageReducer
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
