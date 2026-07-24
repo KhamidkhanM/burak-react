d93130d379bf409484fa21639d6062cff89ee8d7
KhamidkhanM <hoshimalievhamidxonn@gmail.com>
2026-07-25 03:49:42 +0900

    feat: develop productsPage slice and selector

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,17 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import reduxLogger from "redux-logger";
 import homePageReducer from "./screens/homePage/slice";
+import Products from "./screens/productsPage/Products";
+import ProductsPage from "./screens/productsPage";
+import ProductsPageReducer from "./screens/productsPage/slice";
 
 export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     // @ts-ignore
     getDefaultMiddleware().concat(reduxLogger),
   reducer: {
-    homePage: homePageReducer
+    homePage: homePageReducer,
+    productsPage: ProductsPageReducer,
   },
 });
 
